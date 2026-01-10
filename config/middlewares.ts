20d05271c0198ccabe4fae101f39a77580c1ac9d
horsee77 <lucashorsee@icloud.com>
2026-01-10 15:17:03 -0300

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://mediumvioletred-oyster-362212.hostingersite.com',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: '*',
+      credentials: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
