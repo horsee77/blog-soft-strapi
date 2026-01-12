2d735aa352cd5473b41c50677e2bbd20ec95b38e
horsee77 <lucashorsee@icloud.com>
2026-01-12 18:53:25 -0300

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,7 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      connectionString: env('DATABASE_URL'),
       ssl: {
         rejectUnauthorized: false,
       },
